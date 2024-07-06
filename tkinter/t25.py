dcc5dc76a8d0df7119f3c54cf211586db923c911
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2024-07-06 11:55:28 +0530

    Update t25.py

--- tkinter/t25.py
+++ tkinter/t25.py
@@ -3,13 +3,16 @@ from tkinter import *
 root = Tk()
 menu = Menu(root)
 root.config(menu=menu)
-filemenu = Menu(menu)
+
+filemenu = Menu(menu,tearoff=0)
 menu.add_cascade(label='File', menu=filemenu)
 filemenu.add_command(label='New')
 filemenu.add_command(label='Open...')
 filemenu.add_separator()
 filemenu.add_command(label='Exit', command=root.quit)
-helpmenu = Menu(menu)
+
+helpmenu = Menu(menu,tearoff=0)
 menu.add_cascade(label='Help', menu=helpmenu)
 helpmenu.add_command(label='About')
+
 mainloop()
