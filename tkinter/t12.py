0dd068e25ae5efc8be4492019e6d717d927f17e6
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2024-11-21 16:58:22 +0530

    Update t12.py

--- tkinter/t12.py
+++ tkinter/t12.py
@@ -1,20 +1,11 @@
-import tkinter as tk
+from tkinter import *
 from tkinter import ttk
-root = tk.Tk()
+root = Tk()
 root.title("Combobox Example")
 
-def update_label(event):
-    selected_value = combobox.get()
-    label.config(text=f"Selected: {selected_value}")
-
-selected_value = tk.StringVar()
-
-combobox = ttk.Combobox(root, textvariable=selected_value)a
+combobox = ttk.Combobox()
 combobox['values'] = ("PEN", "PENCIL", "BOOK")
 combobox.current(0)  # Set the default selected value
 combobox.pack()
-combobox.bind("<<ComboboxSelected>>", update_label)
-label = tk.Label(root, text=" ")
-label.pack()
 
-root.mainloop()
+mainloop()
