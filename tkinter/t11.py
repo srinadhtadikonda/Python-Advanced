28a909b4de6795f18cd11fe151b4356666345f97
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2025-07-14 10:59:41 +0530

    Update t11.py

--- tkinter/t11.py
+++ tkinter/t11.py
@@ -1,31 +1,26 @@
-import tkinter as tk
+from tkinter import *
 
-# Function to update the label text with the selected radiobutton value
+root = Tk()
+root.title("Radiobutton Example")
+
+radio_var = StringVar()
+radio_var.set("Option 1")
+
 def update_label():
     selected_value = radio_var.get()
     label.config(text=f"Selected: {selected_value}")
 
-# Create the main window
-root = tk.Tk()
-root.title("Radiobutton Example")
 
-# Create a Tkinter variable to store the selected radiobutton value
-radio_var = tk.StringVar()
+radio1 = Radiobutton(root, text="CASH", variable=radio_var, value="CASH", command=update_label)
+radio1.pack(anchor=W)
 
-# Set the default value of the radiobuttons
-radio_var.set("Option 1")
+radio2 = Radiobutton(text="CHEQUE", variable=radio_var, value="CHEQUE", command=update_label)
+radio2.pack(anchor=W)
 
-# Create radiobuttons
-radio1 = tk.Radiobutton(root, text="CASH", variable=radio_var, value="CASH", command=update_label)
-radio1.pack(anchor=tk.W)
+radio3 =Radiobutton(text="DD", variable=radio_var, value="DD", command=update_label)
+radio3.pack(anchor=W)
 
-radio2 = tk.Radiobutton(root, text="CHEQUE", variable=radio_var, value="CHEQUE", command=update_label)
-radio2.pack(anchor=tk.W)
-
-radio3 = tk.Radiobutton(root, text="DD", variable=radio_var, value="DD", command=update_label)
-radio3.pack(anchor=tk.W)
-
-# Create a label to display the selected value
-label = tk.Label(root, text=" ")
+label = Label(text=" ")
 label.pack()
+
 root.mainloop()
