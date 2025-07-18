8a678c63be6fa7e1bb56d437aa26b5384bde6da2
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2025-07-18 16:02:53 +0530

    Create t04.py

---
+++ turtlegraphics/t04.py
@@ -0,0 +1,10 @@
+from turtle import *
+title("Circle Drawing")			#used to set window title
+setup(500, 500)				#used to set window width,height
+bgcolor("red")					#used to set window background color
+color("green")					#used to set shape color
+pencolor("blue")				#used to set line color
+pensize(5)					#used to set line width
+shape("turtle")					#used to set shape
+circle(100,360)				#used to draw circle
+done()
