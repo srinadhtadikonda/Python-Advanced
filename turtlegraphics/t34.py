a1fb1d22ff902c77b4de19cf0d1b24ea7dc29647
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2025-07-19 08:08:25 +0530

    Create t34.py

---
+++ turtlegraphics/t34.py
@@ -0,0 +1,12 @@
+
+from turtle import *
+
+write('Hello, world!')
+forward(80)
+right(45)
+forward(50)
+write('123456789', font=('Arial', 48, 'normal'))
+right(45)
+forward(30)
+write('oOoOoOoOoOo')
+done()
