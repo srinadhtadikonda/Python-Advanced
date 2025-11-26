fa31d66e1d391d9a3207bfb7a40ebf4ef226838c
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2025-11-26 10:45:11 +0530

    Update fb01.py

--- facebook/fb01.py
+++ facebook/fb01.py
@@ -1,8 +1,8 @@
 import json 
 import facebook 
-  
+from facebook import GraphAPI 
 def main(): 
-    token = "Please replace this line with your access token"
+    token = "EAAlZB3bUwW10BQIpWybsWb0OlTZBxUZAsRAVGIkj03HgEWnIsZBGEnRK5DZBZBzlBRjjtVIgYh7Cm5sZBMZBR9M6aFZCgu3YQLD3KpzBI7OF3kurtUm5QSoBetWveOFiExoL3pOM2GeeW8n4JZAbs61OPzGwH7Rpj6NZCrQto9F9r9ZAcsMmXd4A0wqjIp4ToxeJJZAQJNSU4Ap5nPz4d8rxMwjm3dgBR3AnqwP4c"
     graph = facebook.GraphAPI(token) 
     profile = graph.get_object('me', fields ='first_name, gender, birthday, email')  
      
