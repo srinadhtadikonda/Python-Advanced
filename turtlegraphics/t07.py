39f982c17cc0ea3dfe0543be91ebd6109ea00384
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2026-03-25 08:46:02 +0530

    Update t07.py

--- turtlegraphics/t07.py
+++ turtlegraphics/t07.py
@@ -1,18 +1,14 @@
-# import for turtle module
 import turtle
 
-# making a workScreen
 ws = turtle.Screen()
-
-# defining a turtle instance
 geekyTurtle = turtle.Turtle()
 
-# iterating the loop 8 times
+geekyTurtle.speed(3)
+geekyTurtle.color("blue")
+geekyTurtle.pensize(3)
+
 for i in range(8):
-  
-    # moving turtle 100 units forward
     geekyTurtle.forward(100)
-    
-    # turning turtle 45 degrees so 
-    # as to make perfect angle for an octagon
     geekyTurtle.left(45)
+
+turtle.done()
