ee0c7591e90f256164c528c5a22fde98d1315eeb
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2026-06-05 10:54:09 +0530

    Update ins.py

--- database/guiclass/ins.py
+++ database/guiclass/ins.py
@@ -1,11 +1,10 @@
-from tkinter.ttk import *
 from tkinter import *
 import mysql.connector
 from tkinter import messagebox
-mydb = mysql.connector.connect(host="localhost",user="root",password="sriinformatics",database="demobase")
-mycursor = mydb.cursor()
+p=mysql.connector.connect(host="localhost",user="root",password="brilliant",database="demobase",port=3306)
+q=p.cursor()
 root = Tk()
-root.title("office Data")
+root.title("Insert")
 root.geometry("750x350")
 
 label1 = Label(root, text="ENO", width=20,height=2).grid(row=1, column=0)
@@ -13,37 +12,33 @@ label2 = Label(root, text="ENAME", width=20,height=2).grid(row=2, column=0)
 label3 = Label(root, text="ESAL", width=20,height=2).grid(row=3, column=0)
 label4 = Label(root, text="EGRADE", width=20, height=2).grid(row=4, column=0)
 
-
 e1 = Entry(root, width=30, borderwidth=5)
 e1.grid(row=1, column=2)
+
 e2 = Entry(root, width=30, borderwidth=5)
 e2.grid(row=2, column=2)
+
 e3 = Entry(root, width=30, borderwidth=5)
 e3.grid(row=3, column=2)
+
 e4 = Entry(root, width=30, borderwidth=5)
 e4.grid(row=4, column=2)
 
-
-def Register():
-       Insert = "Insert into myemp(eno,ename,esal,egrade) values(%s,%s,%s,%s)"
-       eno = e1.get()
-       ename = e2.get()
-       esal = e3.get()
-       egrade = e4.get()
-       if(eno != "" and ename != "" and esal != ""and egrade != ""):
-            Value = (eno,ename,esal,egrade)
-            mycursor.execute(Insert, Value)
-            mydb.commit()
-            messagebox.askokcancel("Information", "Record inserted")
-            e1.delete(0, 'end')
-            e2.delete(0, 'end')
-            e3.delete(0, 'end')
-            e4.delete(0, 'end')
-       else:
-            if (eno == "" and ename == "" and esal== "" and egrade == ""):
-                messagebox.askokcancel("Information", "New Entery Fill All Details")
-            else:
-                messagebox.askokcancel("Information", "Some fields left blank")
+def insrec():
+    r="insert into myemp(eno,ename,esal,egrade) values (%s,%s,%s,%s)"
+    eno = e1.get()
+    ename = e2.get()
+    esal = e3.get()   
+    egrade=e4.get()
+    s=(eno,ename,esal,egrade)
+    q.execute(r,s)
+    p.commit()
+    messagebox.showinfo("Success","Record Inserted")
+    e1.delete(0,END)
+    e2.delete(0,END)
+    e3.delete(0,END)
+    e4.delete(0,END)
     
-button1 = Button(root, text="Register", width=10, height=2,command=Register).grid(row=5, column=1)
-root.mainloop()
+btn1=Button(text="INSERT",command=insrec)
+btn1.grid(row=5,column=0)
+mainloop()
