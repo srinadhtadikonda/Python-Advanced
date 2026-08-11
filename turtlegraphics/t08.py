764e57687653e3be09aa8249b9bb4b862db1007a
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2026-08-11 16:08:58 +0530

    Update t08.py

--- turtlegraphics/t08.py
+++ turtlegraphics/t08.py
@@ -1,17 +1,5 @@
-# import the turtle modules
-import turtle
-
-# Start a work Screen
-ws = turtle.Screen()
-
-# Define a Turtle Instance
-geekyTurtle = turtle.Turtle()
-
-# executing loop 6 times for 6 sides
+from turtle import *
 for i in range(6):
-
-    # Move forward by 90 units
-    geekyTurtle.forward(90)
-
-    # Turn left the turtle by 300 degrees
-    geekyTurtle.left(300)
+    forward(90)
+    left(300)
+done()
