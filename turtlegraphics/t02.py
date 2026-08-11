22ad7e9ba1a6c24e8a338f02d372cbcd97c4c55d
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2026-08-11 16:04:25 +0530

    Set up turtle graphics window and properties
    Added window title, size, background color, and turtle properties.

--- turtlegraphics/t02.py
+++ turtlegraphics/t02.py
@@ -1,7 +1,9 @@
 from turtle import *
-for i in range(4):
-   forward(100)
-   left(90)
+title("Circle Drawing")			#used to set window title
+setup(500, 500)				#used to set window width,height
+bgcolor("red")					#used to set window background color
+color("green")					#used to set shape color
+pencolor("blue")				#used to set line color
+pensize(5)					#used to set line width
+shape("turtle")					#used to set shape
 done()
-
-
