0cb8e2e1db2ce84c7e4ecb3bdc262f44552d029a
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2026-08-11 16:06:09 +0530

    Update t05.py

--- turtlegraphics/t05.py
+++ turtlegraphics/t05.py
@@ -1,18 +1,7 @@
 
 from turtle import *
-
-title("Circle Drawing")
-setup(500, 500)
-bgcolor("black")
-shape("turtle")   
-pencolor("blue")     # Line color
-fillcolor("cyan")    # Fill color for the shape     # Pen color
-pensize(5)
-speed(1)
-
-# Begin drawing
-begin_fill()
-circle(100, 360)
-end_fill()
-
+i=1
+while(i<=5):
+  forward(100)
+  left(90)
 done()
